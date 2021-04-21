9c1bcbfeb393d14543a9dad9011f3f6f1bfb9d2b
zhenkunl <lizhenk@yeah.net>
2021-04-21 20:11:17 +0800

    change the name of class itself and the return type of a few functions

--- src/string_type_mod.F90
+++ src/string_type_mod.F90
@@ -9,21 +9,29 @@ module string_type_mod
     character(len=:), allocatable :: value
   contains
     private
-    procedure, public, pass(this)  :: get_value
-    procedure, public, pass(this)  :: len      => len_string
-    procedure, public, pass(this)  :: len_trim => len_trim_string
-    procedure, public, pass(this)  :: trim     => trim_string
-    procedure, public, pass(this)  :: adjustl  => adjustl_string
-    procedure, public, pass(this)  :: adjustr  => adjustr_string
-    procedure, public, pass(this)  :: reverse  => resverse_string
-    procedure, public, pass(this)  :: to_lower => to_lower_string
-    procedure, public, pass(this)  :: to_upper => to_upper_string
-    procedure, public, pass(this)  :: to_int   => string_to_int
-    procedure, private, pass(this) :: assign_char
-    procedure, private, pass(this) :: assign_string
+    procedure, public, pass(self)  :: get_value
+    procedure, public, pass(self)  :: len      => len_string
+    procedure, public, pass(self)  :: len_trim => len_trim_string
+    procedure, public, pass(self)  :: trim     => trim_string
+    procedure, public, pass(self)  :: adjustl  => adjustl_string
+    procedure, public, pass(self)  :: adjustr  => adjustr_string
+    procedure, public, pass(self)  :: reverse  => resverse_string
+    procedure, public, pass(self)  :: to_lower => to_lower_string
+    procedure, public, pass(self)  :: to_upper => to_upper_string
+    procedure, public, pass(self)  :: to_int   => string_to_int
+    procedure, public, pass(self)  :: capitalize
+    ! procedure, public, pass(self)  :: count
+    ! procedure, public, pass(self)  :: find
+    ! procedure, public, pass(self)  :: start_with
+    ! procedure, public, pass(self)  :: end_with
+    ! procedure, public, pass(self)  :: join
+    ! procedure, public, pass(self)  :: split
+    ! procedure, public, pass(self)  :: strip
+    procedure, private, pass(lhs)  :: assign_char
+    procedure, private, pass(lhs)  :: assign_string
     generic, public                :: assignment(=) => assign_char, assign_string
 #ifdef __GNUC__
-    procedure, public, pass(this)  :: delete => delete_string_polymorph
+    procedure, public, pass(self)  :: delete => delete_string_polymorph
 #endif
     final :: delete_string
   end type string
@@ -58,168 +66,190 @@ contains
 
   end function new_string_from_int
 
-  subroutine delete_string(this)
+  subroutine delete_string(self)
 
     implicit none
-    type(string), intent(inout) :: this
+    type(string), intent(inout) :: self
 
-    if (allocated(this%value)) then
-      deallocate(this%value)
+    if (allocated(self%value)) then
+      deallocate(self%value)
     end if
 
   end subroutine delete_string
 
 #ifdef __GNUC__
-  subroutine delete_string_polymorph(this)
+  subroutine delete_string_polymorph(self)
 
     implicit none
-    class(string), intent(inout) :: this
+    class(string), intent(inout) :: self
 
-    if (allocated(this%value)) then
-      deallocate(this%value)
+    if (allocated(self%value)) then
+      deallocate(self%value)
     end if
 
   end subroutine delete_string_polymorph
 #endif
 
-  function get_value(this) result(string_value)
+  function get_value(self) result(string_value)
 
     implicit none
-    class(string), intent(in)     :: this
+    class(string), intent(in)     :: self
     character(len=:), allocatable :: string_value
 
-    string_value = this%value
+    string_value = self%value
 
   end function get_value
 
-  function len_string(this) result(length)
+  function len_string(self) result(length)
 
     implicit none
-    class(string), intent(in) :: this
+    class(string), intent(in) :: self
     integer                   :: length
 
-    length = len(this%value)
+    length = len(self%value)
 
   end function len_string
 
-  function len_trim_string(this) result(length)
+  function len_trim_string(self) result(length)
 
     implicit none
-    class(string), intent(in) :: this
+    class(string), intent(in) :: self
     integer                   :: length
 
-    length = len_trim(this%value)
+    length = len_trim(self%value)
 
   end function len_trim_string
 
-  function trim_string(this) result(trimmed_string)
+  function trim_string(self) result(trimmed_string)
 
     implicit none
-    class(string), intent(in)     :: this
-    character(len=:), allocatable :: trimmed_string
+    class(string), intent(in) :: self
+    type(string)              :: trimmed_string
 
-    trimmed_string = trim(this%value)
+    trimmed_string = self
+    trimmed_string%value = trim(self%value)
 
   end function trim_string
 
-  function adjustl_string(this) result(adjusted_string)
+  function adjustl_string(self) result(adjusted_string)
 
     implicit none
-    class(string), intent(in)     :: this
-    character(len=:), allocatable :: adjusted_string
+    class(string), intent(in) :: self
+    type(string)              :: adjusted_string
 
-    adjusted_string = adjustl(this%value)
+    adjusted_string = self
+    adjusted_string%value = adjustl(self%value)
 
   end function adjustl_string
 
-  function adjustr_string(this) result(adjusted_string)
+  function adjustr_string(self) result(adjusted_string)
 
     implicit none
-    class(string), intent(in)     :: this
-    character(len=:), allocatable :: adjusted_string
+    class(string), intent(in) :: self
+    type(string)              :: adjusted_string
 
-    adjusted_string = adjustr(this%value)
+    adjusted_string = self
+    adjusted_string%value = adjustr(self%value)
 
   end function adjustr_string
 
-  function resverse_string(this) result(reverse_string)
+  function resverse_string(self) result(reversed_string)
 
     implicit none
-    class(string), intent(in)     :: this
-    character(len=:), allocatable :: reverse_string
-    integer                       :: i, n
+    class(string), intent(in) :: self
+    type(string)              :: reversed_string
+    integer                   :: i, n
 
-    reverse_string = this%value
-    n = this%len()
+    reversed_string = self
+    n = self%len()
     do i = 1, n
-      reverse_string(n-i+1:n-i+1) = this%value(i:i)
+      reversed_string%value(n-i+1:n-i+1) = self%value(i:i)
     end do
 
   end function resverse_string
 
-  function to_lower_string(this) result(lower_string)
+  function to_lower_string(self) result(lower_string)
 
     implicit none
-    class(string), intent(inout)  :: this
-    character(len=:), allocatable :: lower_string
-    character(len=*), parameter   :: lower = "abcdefghijklmnopqrstuvwxyz"
-    character(len=*), parameter   :: upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    integer                       :: i, j
+    class(string), intent(inout) :: self
+    type(string)                 :: lower_string
+    character(len=*), parameter  :: lower_alphabet = "abcdefghijklmnopqrstuvwxyz"
+    character(len=*), parameter  :: upper_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    integer                      :: i, j
 
-    lower_string = this%value
-    do i = 1, this%len()
-      j = index(upper, lower_string(i:i))
+    lower_string = self
+    do i = 1, self%len()
+      j = index(upper_alphabet, lower_string%value(i:i))
       if (j > 0) then
-        lower_string(i:i) = lower(j:j)
+        lower_string%value(i:i) = lower_alphabet(j:j)
       end if
     end do
+
   end function to_lower_string
 
-function to_upper_string(this) result(upper_string)
+function to_upper_string(self) result(upper_string)
 
     implicit none
-    class(string), intent(inout)  :: this
-    character(len=:), allocatable :: upper_string
-    character(len=*), parameter   :: lower = "abcdefghijklmnopqrstuvwxyz"
-    character(len=*), parameter   :: upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    integer                       :: i, j
+    class(string), intent(inout) :: self
+    type(string)                 :: upper_string
+    character(len=*), parameter  :: lower_alphabet = "abcdefghijklmnopqrstuvwxyz"
+    character(len=*), parameter  :: upper_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    integer                      :: i, j
 
-    upper_string = this%value
-    do i = 1, this%len()
-      j = index(lower, upper_string(i:i))
+    upper_string = self
+    do i = 1, self%len()
+      j = index(lower_alphabet, upper_string%value(i:i))
       if (j > 0) then
-        upper_string(i:i) = upper(j:j)
+        upper_string%value(i:i) = upper_alphabet(j:j)
       end if
     end do
+
   end function to_upper_string
 
-  function string_to_int(this) result (int)
+  function capitalize(self) result(capitalized_string)
 
     implicit none
-    class(string), intent(inout) :: this
+    class(string), intent(inout) :: self
+    type(string)                 :: capitalized_string
+    character(len=*), parameter  :: lower_alphabet = "abcdefghijklmnopqrstuvwxyz"
+    character(len=*), parameter  :: upper_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    integer                      :: i
+
+    capitalized_string = self%to_lower()
+    i = index(lower_alphabet, capitalized_string%value(1:1))
+    if (i > 0) then
+      capitalized_string%value(1:1) = upper_alphabet(i:i)
+    end if
+
+  end function capitalize
+
+  function string_to_int(self) result (int)
+
+    implicit none
+    class(string), intent(inout) :: self
     integer                      :: int
 
-    read(this%value, *) int
+    read(self%value, *) int
 
   end function string_to_int
 
-  subroutine assign_char(this, str)
+  subroutine assign_char(lhs, rhs)
 
     implicit none
-    class(string), intent(inout) :: this
-    character(*), intent(in)     :: str
+    class(string), intent(inout) :: lhs
+    character(*), intent(in)     :: rhs
 
-    this%value = str
+    lhs%value = rhs
 
   end subroutine assign_char
 
-  subroutine assign_string(this, from)
+  subroutine assign_string(lhs, rhs)
 
     implicit none
-    class(string), intent(inout) :: this
-    type(string), intent(in)     :: from
+    class(string), intent(inout) :: lhs
+    type(string), intent(in)     :: rhs
 
-    this%value = from%value
+    lhs%value = rhs%value
 
   end subroutine assign_string
 
--- src/test_string_type.F90
+++ src/test_string_type.F90
@@ -3,8 +3,7 @@ program test_string_type
   use string_type_mod
   implicit none
 
-  type(string) :: str, str2
-  character(len=:), allocatable :: str3
+  type(string) :: str, str2, str3
 
   print*, '-', str%get_value(), '-', str%len(), str%len_trim()
   str = string("123 ")
@@ -15,15 +14,17 @@ program test_string_type
   print*, str%get_value(), str%len(), str%len_trim()
   str = " 12 34 "
   print*, str%to_int()
-  print*, str%reverse()
-  print*, str%get_value(), str%len(), str%len_trim()
-  str2 = string("That Is ")
-  print*, str2%to_lower(), str2%to_upper()
+  str2 = str%reverse()
   print*, str2%get_value(), str2%len(), str2%len_trim()
-  str = str2
-  print*, str%get_value(), str%len(), str%len_trim()
+  str = string("that Is ")
+  str2 = str%to_lower()
+  print*, str2%get_value(), str2%len(), str2%len_trim()
+  str2 = str%to_upper()
+  print*, str2%get_value(), str2%len(), str2%len_trim()
+  str2 = str%capitalize()
+  print*, str2%get_value(), str2%len(), str2%len_trim()
   str3 = str%trim()
-  print*, str3, len(str3), len_trim(str3)
+  print*, str3%get_value(), str3%len(), str3%len_trim()
 #ifdef __GNUC__
   call str%delete()
 #endif
