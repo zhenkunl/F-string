b5b4ef7fbfe6b094973a45d245f83638afd70722
zhenkunl <lizhenk@yeah.net>
2021-04-27 15:37:00 +0800

    update test program

--- src/test_string_type.F90
+++ src/test_string_type.F90
@@ -3,9 +3,7 @@ program test_string_type
   use string_type_mod
   implicit none
 
-  type(string) :: str1, str2, str3, str4, str5
-  character(*), parameter :: chars(3) = ["123", "456", "789"]
-  type(string) :: strs(3)
+  type(string) :: str1, str2, str3, str4
   character(:), allocatable :: characters
 
   print*, '-', str1%get_value(), '-', str1%len(), str1%len_trim()
@@ -34,23 +32,15 @@ program test_string_type
   str1 = "123log.txt"
   print*, str1%start_with("123"), str1%start_with("og", 5, 15)
   print*, str1%end_with(".txt"), str1%end_with("txt", 11, 20)
-  str4 = '-'
-  str5 = str4%join(chars)
-  print*, str5%get_value(), str5%len(), str5%len_trim()
   str1 = "123"
   str2 = "456"
   str3 = "789"
-  strs(1) = str1
-  strs(2) = str2
-  strs(3) = str3
-  str5 = str4%join(strs)
-  print*, str5%get_value(), str5%len(), str5%len_trim()
-  str5 = str3//"098"
-  print*, str5%get_value(), str5%len(), str5%len_trim()
-  str5 = str1//str3
-  print*, str5%get_value(), str5%len(), str5%len_trim()
-  str5 = "098"//str3
-  print*, str5%get_value(), str5%len(), str5%len_trim()
+  str4 = str3//"098"
+  print*, str4%get_value(), str4%len(), str4%len_trim()
+  str4 = str1//str3
+  print*, str4%get_value(), str4%len(), str4%len_trim()
+  str4 = "098"//str3
+  print*, str4%get_value(), str4%len(), str4%len_trim()
   characters = str1
   print*, characters
   print*, str1 == str2, str1 /= str2
@@ -64,6 +54,5 @@ program test_string_type
   call str2%delete()
   call str3%delete()
   call str4%delete()
-  call str5%delete()
 #endif
 end program test_string_type
